658bbd5fdfd6ee22422dc5981f5154f96faaad49
Manuel Stathis <manuel>
2018-03-20 23:36:20 +0100

    spin1.dat can be visualised. some errors with loops edges can happen. fix it!

--- Analysis_Triangular.py
+++ Analysis_Triangular.py
@@ -5,21 +5,21 @@ import matplotlib.pyplot as plt
 
 # init graphs
 S = nx.Graph()
-SpinData = np.genfromtxt('Outputfiles/Spin1.dat',dtype=float, delimiter="\t")
-Spin_val = SpinData[:, 2]
+SpinData = np.genfromtxt('Outputfiles/Spin2.dat', dtype=float, delimiter="\t")
 
 i_max = max(SpinData[:, 0])
 j_max = max(SpinData[:, 1])
-rows = range(int(i_max)+1)
-cols = range(int(j_max)+1)
+rows = range(int(i_max) + 1)
+cols = range(int(j_max) + 1)
 
 Spin_pos = zip(SpinData[:, 0], SpinData[:, 1])
 Spin_val = SpinData[:, 2]
-Spin_couple = {pos: Spin_val[ind] for ind, pos in enumerate(Spin_pos)}
-S.add_nodes_from(Spin_pos)
-nx.set_node_attributes(S, 'spin', Spin_couple) # apparently they aren't set...
+for ind, s_pos in enumerate(Spin_pos):
+    S.add_node(s_pos, spin=Spin_val[ind])
 
-# no periodic bc at the moment
+nx.set_node_attributes(S, 'none', 'color')
+
+# no periodic bc at the moment some error potential
 for i in rows:
     for j in cols:
         if i < i_max:
@@ -33,29 +33,23 @@ for i in rows:
 for i in rows:
     for j in cols:
         x = (j * 0.5) + i
-        y = j * math.sqrt(3)/2
+        y = j * math.sqrt(3) / 2
         S.node[(i, j)]['pos'] = (x, y)
 
-positions = nx.get_node_attributes(S, 'pos')
-nx.draw(S, positions)
+remove = [node for node, data in S.nodes(data=True) if data['spin'] == 0]
+S.remove_nodes_from(remove)
 
-'''
-ii = (i for i in cols for j in rows)
-jj = (j for i in cols for j in rows)
-xx = (0.5 * (j % 2) + i for i in cols for j in rows)
-h = math.sqrt(3) / 2
-yy = (h * j + .01 * i * i for i in cols for j in rows)
-pos = {(i, j): (x, y) for i, j, x, y in zip(ii, jj, xx, yy) if (i, j) in S}
-
-nx.set_node_attributes(S, pos, 'pos')
-'''
-
+color_map = []
+for node, data in S.nodes(data=True):
+    if data['spin'] == 1:
+        color_map.append('b')  # blue color
+    elif data['spin'] == -1:
+        color_map.append('r')  # red color
 
 # Draw
-# pos = dict((n, n) for n in S.nodes())
-# labels = dict(((i, j), i * 10 + j) for i, j in S.nodes())
 
+positions = nx.get_node_attributes(S, 'pos')
+
+nx.draw(S, positions, node_color=color_map)
 plt.axis('equal')
 plt.show()
-
-
