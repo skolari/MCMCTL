9256695cfecc763e40a6997699bfd4b200e52779
Manuel Stathis <manuel>
2018-03-20 19:54:45 +0100

    first triangular lattice

--- DirMCMC.bat
+++ DirMCMC.bat
@@ -1,3 +1,3 @@
 start cmd /k echo Hello, Manuel!
 cd "C:\Users\Manuel Stathis\Documents\EPFL\PhysikMA2\Projet\MCMC - Triangular Lattice"
-.\Triangular_Lattice_py\Scripts\activate.bat
+start "Project Ising" .\Triangular_Lattice_py\Scripts\activate.bat
