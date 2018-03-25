e8145ff3fd6e20df2283d04dcef9f5f255e4e2d5
Manuel Stathis <manuel>
2018-03-25 15:58:51 +0200

    possible to draw dual lattice. but problems with periodic bc.

--- Analysis_Triangular.py
+++ Analysis_Triangular.py
@@ -2,8 +2,8 @@ from TriangularLattice import TriangularLattice
 from DualLattice import DualLattice
 
 filename = 'Outputfiles/Spin1.dat'
-filename2 = 'Outputfiles/DimerAdj1.dat'
+filename2 = 'Outputfiles/DimerAdj2.dat'
 triangular_lattice = TriangularLattice(filename)
 dual_lattice = DualLattice(filename2)
-# triangular_lattice.draw_tri_lattice()
+#  triangular_lattice.draw_tri_lattice()
 dual_lattice.draw_dual_lattice()
