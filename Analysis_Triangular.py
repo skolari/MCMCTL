95f6a233267903c90050cc568a18bfb61d0364aa
Manuel Stathis <manuel>
2018-04-01 12:42:34 +0200

    new Spin struct next commit

--- Analysis_Triangular.py
+++ Analysis_Triangular.py
@@ -4,5 +4,5 @@ from DrawPlot import DrawPlot
 filename_s = 'Outputfiles/Spin11.dat'
 filename_d = 'Outputfiles/DimerAdj11.dat'
 draw_plot = DrawPlot(filename_s, filename_d)
-draw_plot.draw_spin_and_dual_lattice()
+draw_plot.draw_spin_lattice()
 # fehler entdeckt mit boundary...
